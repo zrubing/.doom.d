9b6db2ebf102ab5c42b0d7133c721a1de95d7260
qdx6dcwnj <qdx6dcwnj@mozmail.com>
2025-01-31 00:43:20 +0800

    feat: Configure Aider package with OpenAI API settings in Emacs Lisp

---
+++ aider.el
@@ -0,0 +1,11 @@
+
+
+(use-package aider
+  :config
+  (setq aider-args '("--model" "openai/gpt-4o-mini"))
+  (setenv "OPENAI_API_BASE" "https://chatapi.onechats.top/v1/")
+  (let ((api-key (funcall (plist-get (car (auth-source-search :host "chatapi.onechats.top")) :secret))))
+   (setenv "OPENAI_API_KEY" (encode-coding-string api-key 'utf-8)))
+
+  (global-set-key (kbd "C-c a") 'aider-transient-menu))
+(provide 'aider.el)
