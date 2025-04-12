80f8d51e44e0d5a5ec2f2fae61a038c1a889fb0b
zrubing <rubingem@gmail.com>
2025-04-12 17:51:12 +0800

    Specify librime location

--- config.el
+++ config.el
@@ -187,7 +187,8 @@
   :init
   (setq rime-user-data-dir "~/.config/rime"
         rime-share-data-dir "~/.local/share/rime-data/share/rime-data")
-  ;; (setq rime-emacs-module-header-root "~/.nix-profile/include"
+  (setq rime-emacs-module-header-root "~/.local/share/emacs/librime/include"
+        rime-librime-root "~/.local/share/emacs/librime")
   ;;       rime-librime-root "~/.nix-profile"
   ;;       rime-share-data-dir "~/.local/share/rime-data/share/rime-data")
   :custom
